189cf89c40b8b03797c631db8e77bdebc8734650
Johnathon Scott <js573712@gmail.com>
2023-08-26 17:26:30 -0400

    add test-isElevated function

--- profileModule.psm1
+++ profileModule.psm1
@@ -1,4 +1,17 @@
-# Implement your module commands in this script.
+function test-isElevated
+{
+    $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $p = New-Object System.Security.Principal.WindowsPrincipal($id)
+
+    if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
+    { 
+        Write-Output $true 
+    }      
+    else
+    { 
+        Write-Output $false
+    }   
+}
 function connect-exo{
    connect-exchangeonline
 <# I switched to using the new module (exchangeonlinemanagement)
